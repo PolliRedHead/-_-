3b27ecec9fdb2af5a043d1bc7afa765f5c433603
PolliRedHead <polinaryzh@yandex.ru>
2017-04-19 20:08:08 +0700

    New files

---
+++ БД_ПР_ЗАПРОСЫ.sql
@@ -0,0 +1,36 @@
+﻿select LAST_NAME
+from employees
+where id_title=(
+                                select id
+                                from title
+                                where name='KASSIR');
+
+
+select SUM(count(id_prod))
+from check_c
+where id_prod=ANY(
+                select id
+                from product
+                where id_producer=(
+                                    select id
+                                    from producer
+                                    where id=56))
+group by id_prod;
+
+
+
+select name
+from city
+where id=ANY(
+          select id_city
+          from distributor
+          where id=ANY(
+                    select id_distr
+                    from supply_info
+                    where id = 3698 AND id_emp=ANY(
+                                  select id
+                                  from employees
+                                  where id_title=ANY(
+                                                  select id
+                                                  from title
+                                                  where name='TAKER'))));
